c6652c4e7c57c9339672c96fa7005149767c3e47
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-05-10 14:42:42 +0530

    added code for delete conntact

--- src/main/java/com/bl/address_book/AddContact.java
+++ src/main/java/com/bl/address_book/AddContact.java
@@ -11,13 +11,11 @@ public class AddContact {
 
         // Collection<AddressBook> a = new ArrayList<AddressBook>();
         List<AddressBookMain> a = new ArrayList<AddressBookMain>();
-
-        Aplication1 a2 = new Aplication1();
         Scanner s1 = new Scanner(System.in);
         Scanner sc = new Scanner(System.in);
         Scanner s2 = new Scanner(System.in);
         int ch;
-        do  {
+        do {
             System.out.println("Press 1 for Insert");
             System.out.println("Press 2 for Display");
             System.out.println("Press 3 for Search");
@@ -28,19 +26,79 @@ public class AddContact {
             ch = s1.nextInt();
             switch (ch) {
                 case 1:
-                    a2.insertion();
+                    System.out.print("Enter Serial number: ");
+                    int srnum = s1.nextInt();
+                    System.out.print("Enter First name: ");
+                    String first_name = sc.nextLine();
+                    System.out.print("Enter last name: ");
+                    String last_name = sc.nextLine();
+                    System.out.print("Enter address: ");
+                    String address = sc.nextLine();
+                    System.out.print("Enter state: ");
+                    String state = sc.nextLine();
+                    System.out.print("Enter zip: ");
+                    int zip_code = s1.nextInt();
+                    System.out.print("Enter phone: ");
+                    long phone_number = s2.nextLong();
+                    System.out.print("Enter email: ");
+                    String email_id = sc.nextLine();
+                    a.add(new AddressBookMain(srnum, first_name, last_name, address, state, zip_code, phone_number, email_id));
                     break;
 
                 case 2:
-                    a2.display();
+                    // System.out.println(a);
+                    System.out.println("*************");
+                    Iterator<AddressBookMain> i = a.iterator();
+                    while (i.hasNext()) {
+                        AddressBookMain addr = i.next();
+                        System.out.println(addr);
+                    }
+                    System.out.println("*************");
                     break;
 
                 case 3:
-                    a2.search();
+                    boolean found = false;
+                    System.out.println("Enter serial number to search: ");
+                    srnum = s1.nextInt();
+                    System.out.println("*************");
+                    i = a.iterator();
+                    while (i.hasNext()) {
+                        AddressBookMain addr = i.next();
+                        if (addr.getSrnum() == srnum) {
+                            System.out.println(addr);
+                            found = true;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Entered Number Record Not matched");
+                    }
+                    System.out.println("*************");
                     break;
 
+                case 4:
+                    found = false;
+                    System.out.println("Enter serial number to delete: ");
+                    srnum = s1.nextInt();
+                    System.out.println("*************");
+                    i = a.iterator();
+                    while (i.hasNext()) {
+                        AddressBookMain addr = i.next();
+                        if (addr.getSrnum() == srnum) {
+                            i.remove();
+                            found = true;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Entered Name Record Not matched");
+                    } else {
+                        System.out.println("Record Deleted Successfully");
+                    }
+                    System.out.println("*************");
+                    break;
 
             }
         } while (ch != 0);
+
     }
+
         }
