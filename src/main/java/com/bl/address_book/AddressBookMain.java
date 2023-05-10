01b6e91aedac131b6fe283ef8dba0904004b3bd7
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-05-10 13:28:49 +0530

    added welcome msg to master branch

---
+++ src/main/java/com/bl/address_book/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bl.address_book;
+
+public class AddressBookMain
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address_Book Project");
+    }
+}
